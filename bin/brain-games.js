a36e1d379a818fab503579715b413e19fdfa13a0
M4X <maxprd@M4X.localdomain>
2021-10-21 17:20:54 +0300

    Added src to bin

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
 console.log('Welcome to the Brain Games!');
+import readlineSync from 'readline-sync';
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + userName + '!');
