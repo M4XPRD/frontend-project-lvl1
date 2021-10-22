e6b395867e9bbe99d831eb44e07e0b2dfe9a3add
M4X <maxprd@M4X.localdomain>
2021-10-23 00:43:27 +0300

    Changed code in brain-even

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,6 +2,10 @@ console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const evenNumber = (num) => num % 2 === 0;
 
-const randomNumber = (min, max = 20) => {
-	return Math.random() * (max - min) + min;
+const getRandomIntInclusive = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+const question = `${'Question:'}${getRandomIntInclusive(0, 30)}`;
