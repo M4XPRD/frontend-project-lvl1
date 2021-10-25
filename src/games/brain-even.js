1117ea090e707116ca095e3cfbd9f2766ea06678
M4X <maxprd@M4X.localdomain>
2021-10-25 18:33:27 +0300

    Edited brain-even v5

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -6,28 +6,36 @@ console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const brainEven = () => {
-	let rightAnswerRow = 0;
-const getRandomIntInclusive = (min, max) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
-
-const question = `${'Question:'}${getRandomIntInclusive(0, 30)}`;
-
-const playerAnswer = readline.Sync.question('Your answer: ');
-
-if (getRandomInclusive(0, 30) % 2 === 0) {
-	if (playerAnswer === 'yes') {
-		rightAnswerRow += 1;
-		return 'Correct!';
-	} else {
-		console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
-		console.log(`Let's try again, ${userName}!`);
-		return;
-	} if (rightAnswerRow === 3) {
-		return `Congratulations, ${username}`;
-}
+  for (let i = 0; i < 4; i += 1) {
+    const getRandomIntInclusive = Math.floor(Math.random() * 20);
+    const question = `Question: ${getRandomIntInclusive}`;
+    console.log(question);
+    const playerAnswer = readlineSync.question('Your answer: ');
+    let rightAnswerRow = 0;
+    if (getRandomIntInclusive % 2 === 0) {
+      if (playerAnswer === 'yes') {
+        rightAnswerRow += 1;
+        console.log('Correct!');
+        const question = `Question: ${getRandomIntInclusive}`;
+        console.log(question);
+      } if (getRandomIntInclusive % 2 !== 0) {
+        if (playerAnswer === 'no') {
+          rightAnswerRow += 1;
+          console.log('Correct!');
+          const question = `Question: ${getRandomIntInclusive}`;
+          console.log(question);
+        }
+      } else {
+        rightAnswerRow = 0;
+        console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+        console.log(`Let's try again, ${userName}!`);
+        const question = `Question: ${getRandomIntInclusive}`;
+        console.log(question);
+      } if (rightAnswerRow === 3) {
+        console.log(`Congratulations, ${username}!`);
+      }
+    }
+  }
 };
 
 export default brainEven;
