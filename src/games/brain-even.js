7cff34bdbf1da8cb4cc763563d8479f036b894fc
M4X <maxprd@M4X.localdomain>
2021-10-25 19:31:04 +0300

    Edited brain-games v6

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,33 +1,33 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May i have your name? ');
-console.log(`Hello, ${userName}!`);
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
 const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May i have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  let rightAnswerRow = 0;
+
   for (let i = 0; i < 4; i += 1) {
     const getRandomIntInclusive = Math.floor(Math.random() * 20);
     const question = `Question: ${getRandomIntInclusive}`;
     console.log(question);
     const playerAnswer = readlineSync.question('Your answer: ');
 
-    let rightAnswerRow = 0;
-
     if (getRandomIntInclusive % 2 === 0 && playerAnswer === 'yes') {
       rightAnswerRow += 1;
       console.log('Correct!');
-    } else {
+    } if (getRandomIntInclusive % 2 === 0 && playerAnswer === 'no') {
       console.log(`"no" is wrong answer ;(. Correct answer was "yes".\nLet's try again, ${userName}!`);
       return;
     } if (getRandomIntInclusive % 2 !== 0 && playerAnswer === 'no') {
       rightAnswerRow += 1;
       console.log('Correct!');
-    } else {
+    } if (getRandomIntInclusive % 2 !== 0 && playerAnswer === 'yes') {
       console.log(`"yes" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}!`);
       return;
     } if (rightAnswerRow === 3) {
-      console.log(`Congratulations, ${username}!`);
+      return console.log(`Congratulations, ${userName}!`);
     }
   }
 };
