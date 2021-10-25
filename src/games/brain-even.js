07b3744927f3e06c5dddc5886e3381c5c4618dbd
M4X <maxprd@M4X.localdomain>
2021-10-25 15:22:06 +0300

    Edited brain-even v1

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,3 +1,8 @@
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May i have your name? ');
+console.log(Hello, ${userName}!);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 const evenNumber = (num) => num % 2 === 0;
@@ -9,3 +14,5 @@ const getRandomIntInclusive = (min, max) => {
 };
 
 const question = `${'Question:'}${getRandomIntInclusive(0, 30)}`;
+
+
