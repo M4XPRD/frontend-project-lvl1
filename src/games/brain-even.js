381d11d03bd2001810acac72e49e5cd3433850b2
M4X <maxprd@M4X.localdomain>
2021-10-26 17:26:41 +0300

    A small fix of brain-even grammar

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const brainEven = () => {
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
