207fdb4227c980d98f02457cca99dd0615ffa50a
M4X <maxprd@M4X.localdomain>
2021-11-02 18:50:38 +0300

    Small fix to prain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,7 +2,7 @@ import gameEngine from '../index.js';
 import getRndInteger from '../numberRandomiser.js';
 
 const isPrime = (number) => {
-  for (let i = 2; i < number / 2; i += 1) {
+  for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return 'no';
     }
