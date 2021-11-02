61680068065cf1760cf07d0d610ecbe4d84fdf60
M4X <maxprd@M4X.localdomain>
2021-11-03 02:30:23 +0300

    Brain-even has been refactored

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,35 +1,22 @@
-import readlineSync from 'readline-sync';
+import gameEngine from '../index.js';
 import getRndInteger from '../numberRandomiser.js';
 
-const brainEven = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have your name? ');
-  console.log(`Hello, ${userName}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const gameRule = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  let rightAnswerRow = 0;
-
-  for (let i = 0; i < 4; i += 1) {
-    const getRandomIntInclusive = getRndInteger(1, 20);
-    const question = `Question: ${getRandomIntInclusive}`;
-    console.log(question);
-    const playerAnswer = readlineSync.question('Your answer: ');
-
-    if (getRandomIntInclusive % 2 === 0 && playerAnswer === 'yes') {
-      rightAnswerRow += 1;
-      console.log('Correct!');
-    } if (getRandomIntInclusive % 2 === 0 && playerAnswer !== 'yes') {
-      return console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "yes".\nLet's try again, ${userName}!`);
-    } if (getRandomIntInclusive % 2 !== 0 && playerAnswer === 'no') {
-      rightAnswerRow += 1;
-      console.log('Correct!');
-    } if (getRandomIntInclusive % 2 !== 0 && playerAnswer !== 'no') {
-      return console.log(`"${playerAnswer}" is wrong answer ;(. Correct answer was "no".\nLet's try again, ${userName}!`);
-    } if (rightAnswerRow === 3) {
-      return console.log(`Congratulations, ${userName}!`);
-    }
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    return true;
   }
-  return console.log();
+  return false;
 };
+
+const getGameData = () => {
+  const randomNumber = getRndInteger(0, 20);
+  const gameQuestion = randomNumber;
+  const gameAnswer = String(isEven(gameQuestion) ? 'yes' : 'no');
+  return [gameQuestion, gameAnswer];
+};
+
+const brainEven = () => gameEngine(gameRule, getGameData);
 
 export default brainEven;
