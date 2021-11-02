0419ce81afab0742a1e6a480844dcdf2501a5760
M4X <maxprd@M4X.localdomain>
2021-11-02 23:14:51 +0300

    Fixed gcd function

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,15 +2,10 @@ import gameEngine from '../index.js';
 import getRndInteger from '../numberRandomiser.js';
 
 const findGCD = (num1, num2) => {
-  if (num1 === 0) {
-    return num2;
-  } if (num2 === 0) {
+  if (num2 === 0) {
     return num1;
-  } let i = num1;
-  while (num1 % i !== 0 || num2 % i !== 0) {
-    i -= 1;
-  }
-  return i;
+  } 
+ return findGCD(num2, num1 % num2);
 };
 
 const getGameData = () => {
