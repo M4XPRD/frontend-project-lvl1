0aa5bd049bf43a1ee45037dbcfece4af62aec3a0
M4X <maxprd@M4X.localdomain>
2021-11-03 01:34:07 +0300

    Fixed isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,16 +6,16 @@ const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 const isPrime = (number) => {
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
-      return 'no';
+      return false;
     }
   }
-  return 'yes';
+  return true;
 };
 
 const getGameData = () => {
   const randomNumber = getRndInteger(2, 100);
   const gameQuestion = randomNumber;
-  const gameAnswer = String(isPrime(randomNumber));
+  const gameAnswer = String(isPrime(randomNumber) ? 'yes' : 'no');
   return [gameQuestion, gameAnswer];
 };
 
