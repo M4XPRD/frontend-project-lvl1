76881bad61fa2d6cb466fa1f25cc88bbe869e418
M4X <maxprd@M4X.localdomain>
2021-11-04 18:22:51 +0300

    Fixed isPrime function

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,6 +4,9 @@ import getRndInteger from '../numberRandomiser.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
   for (let i = 2; i <= number / 2; i += 1) {
     if (number % i === 0) {
       return false;
