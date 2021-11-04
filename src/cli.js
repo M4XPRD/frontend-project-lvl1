74c5f2dd38b3ace740cb52d64e4818428115f34b
M4X <maxprd@M4X.localdomain>
2021-11-04 18:31:17 +0300

    Fixed the function in cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,8 @@
 import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!');
-
-const userName = readlineSync.question('May I have your name? ');
-
-export default () => {
-  console.log(`Hello, ${userName}!`);
+const findPlayerName = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
 };
+
+export default findPlayerName;
